fe49de46ce3cc2836c09ddbf493fe4ac49e3807b
calissk@gmail.com <Dragon69>
2019-08-25 15:34:24 +0200

    Update read.php

--- Fournisseur/read.php
+++ Fournisseur/read.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php
 
-    <link rel="stylesheet" media="screen" type="text/css" href="css/style.css"/>
-    
-    <title>Les fournisseurs</title>
-    <h1>Voici l'ensemble des fournisseurs</h1>
-</head>
-<body>
-/*<?php
 // required header
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -30,17 +18,14 @@ $fournisseur = new Fournisseur($db);
 // query fournisseurs
 $stmt = $fournisseur->read();
 $num = $stmt->rowCount();
- 
+
 // check if more than 0 record found
 if($num>0){
  
-    // products array
+    // fournisseurs array
     $fournisseurs_arr=array();
     $fournisseurs_arr["Enregistrements"]=array();
- 
     // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         // extract row
         // this will make $row['name'] to
@@ -50,9 +35,9 @@ if($num>0){
         $fournisseur_item=array(
             "Identifiant du Fournisseur" => $idFournisseur,
             "Nom du fournisseur" => $nomFournisseur,
-            "Adresse du fournisseur" =>$adresseFournisseur
+            "Adresse du fournisseur"=>$adresseFournisseur,
         );
- 
+        
         array_push($fournisseurs_arr["Enregistrements"], $fournisseur_item);
         
     }
@@ -61,21 +46,18 @@ if($num>0){
     http_response_code(200);
  
     // show fournisseurs data in json format
-    echo json_encode($fournisseurs_arr);
+    echo "Liste de l'ensemble des Fournisseurs ", json_encode($fournisseurs_arr), "\n";
 }
- 
 else{
  
     // set response code - 404 Not found
     http_response_code(404);
  
-    // tell the user no categories found
+    // tell the user no fournisseur found
     echo json_encode(
-        array("message" => "No categories found.")
+        array("message" => "Il n'y a pas de fournisseur trouvé")
     );
 
 
 }
-?>*/
-</body>
-</html>
+?>
