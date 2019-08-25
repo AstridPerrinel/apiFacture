036a385009d7e60513b3198a97fa0f83e01a0026
calissk@gmail.com <Dragon69>
2019-08-25 15:34:29 +0200

    Create affichageFournisseur.php

---
+++ Fournisseur/affichageFournisseur.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" media="screen" type="text/css" href="../css/style.css"/>
+    <link href="../Fournisseur/read.php">
+    <title>Fournisseurs</title>
+    <h2>Voici l'ensemble des fournisseurs</h2>
+</head>
+<body>
+<a href="http://127.0.0.1/apiFacture/accueil.php"><input class="favorite styled"
+       type="button"
+       value="retourner à l'accueil"></a>
+
+</body>
+</html>
